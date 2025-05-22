3e35973c0325d3f3b8aaaee9b77b4f198eed0a71
Tran Minh Huy <tranminhhuy@example.com>
2025-05-22 20:13:19 +0700

    a18z

--- product_management/routes/admin/index.route.js
+++ product_management/routes/admin/index.route.js
@@ -3,6 +3,7 @@ const systemConfig=require("../../config/system")
 const productRoutes=require("./product.route")
 const productCategoryRoutes=require("./product.category.route")
 const RoleRoutes=require("./role.route.js")
+const accountRoutes=require("./account.route")
 
 module.exports=(app)=>{
   const PATH_ADMIN=systemConfig.prefixAdmin
@@ -10,5 +11,6 @@ module.exports=(app)=>{
   app.use(PATH_ADMIN+"/products", productRoutes)
   app.use(PATH_ADMIN+"/products-category",productCategoryRoutes)
   app.use(PATH_ADMIN+"/roles", RoleRoutes)
+  app.use(PATH_ADMIN+`/accounts`, accountRoutes)
 
 }   
