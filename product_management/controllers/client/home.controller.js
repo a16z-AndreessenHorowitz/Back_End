18f879ef68c09cb82f2f26792bc7ba086a15fadf
Tran Minh Huy <tranminhhuy@example.com>
2025-06-08 17:58:55 +0700

    Hiển thị menu chung bên client

--- product_management/controllers/client/home.controller.js
+++ product_management/controllers/client/home.controller.js
@@ -1,6 +1,9 @@
+const ProductCateGory=require("../../models/product-category.model")
+
 // [GET] /
-module.exports.index=(req, res)=>{
+module.exports.index= async(req, res)=>{
+   
     res.render("client/pages/home/index",{
-        pageTitle:"Trang chủ"
+        pageTitle:"Trang chủ",
     })
 }
--- product_management/routes/client/index.route.js
+++ product_management/routes/client/index.route.js
@@ -1,8 +1,13 @@
 const homeRoutes=require("./home.route")
 const productRoutes=require("./product.route")
+const categoryMiddleware=require("../../middlewares/client/category.middleware")
 
 
 module.exports=(app)=>{
+  app.use(categoryMiddleware.category)
+  //tất cả điều sử dụng dạnh mục này
+  // bên admin cần có bảo mật nên sử dụng theo cách khác 
+
   app.use("/",homeRoutes)
 
   app.use("/products",productRoutes)
