bf6557d663e72db310ea6363bb7a5031fb1c504d
Trần Minh Huy <justhuytran@MacBook-Air-cua-Tran.local>
2025-07-06 19:36:38 +0700

    Chèn icon

--- product_management/public/js/chat.js
+++ product_management/public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
+
 // CLIENT_SEND_MESSAGE
 const formSendData=document.querySelector(".chat .inner-form")
 if(formSendData){
@@ -51,3 +53,26 @@ if(bodyChat){
   bodyChat.scrollTop=bodyChat.scrollHeight//cách top đúng bằng chiều cao của scroll
 
 }
+
+//show icon chat
+  const buttonIcon = document.querySelector('.button-icon')
+  if(buttonIcon){
+    const tooltip = document.querySelector('.tooltip')
+    Popper.createPopper(buttonIcon, tooltip)
+    buttonIcon.onclick = () => {
+    tooltip.classList.toggle('shown')
+  }
+  }
+//end show icon
+
+// Insert icon to input 
+const emojiPicker=document.querySelector('emoji-picker')
+if(emojiPicker){
+  const input=document.querySelector(".chat .inner-form input[name='content']")
+
+  emojiPicker.addEventListener("emoji-click",event=>{
+    const icon=event.detail.unicode
+    input.value=input.value+icon; //Có tin nhắn trước đó
+  })
+}
+// end Insert icon to input 
